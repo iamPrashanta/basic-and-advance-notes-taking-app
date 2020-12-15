9ecd690a22496d357f3e1f348eb1ee26e7809258
Prashanta mondal <iloveprashanta@gmail.com>
2020-12-15 14:29:55 +0530

    files

--- advance-version/second.js
+++ advance-version/second.js
@@ -10,43 +10,52 @@ notesInput.addEventListener('keyup', (e) => {
 
 // Load old Notes
 document.addEventListener('DOMContentLoaded', getNotesFromLoc);
+var locNots = [];
 function getNotesFromLoc() {
-    var local_notes = [];
-    if (localStorage.getItem('myNots') === null) {
+    if (localStorage.getItem('myNots') == null) {
         console.log("no previous item in localStorage");
     } else {
-        console.log('yes yes there is something in localStorage');
-        local_notes.push(JSON.parse(localStorage.getItem('myNots')));
+        console.log('yes yes there is something in localStorage\n I Updated the UI');
+        locNots.push(JSON.parse(localStorage.getItem('myNots')));
+        //Printout each note Inside UI
+        locNots[0].forEach(function (each_note) {
+            let newList = document.createElement('li');
+            newList.setAttribute('id', 'notes');
+            newList.innerHTML = each_note + ' <i class="fa fa-close"></i>';
+            list.appendChild(newList);
+        })
     }
-    local_notes[0].forEach(function (each_note) {
-        console.log(each_note);
-        let newList = document.createElement('li');
-        newList.setAttribute('id', 'notes');
-        newList.innerHTML = each_note + ' <i class="fa fa-close"></i>';
-        list.appendChild(newList);
-    })
-}
 
+}
+// console.log(locNots[0].length);
 var notes = [];
 function saveToLocal() {
     if (notesInput.value == 0) {
-        alert('write something\nInput field is blank');
+        alert('Write Something\nInput field is blank');
     } else {
+        getNotesFromLoc();
         notes.push(notesInput.value);
+        // local_notes[0].push(notesInput.value);
+        // console.log(local_notes[0]);
         let newList = document.createElement('li');
         newList.setAttribute('id', 'notes');
         newList.innerHTML = notesInput.value + ' <i class="fa fa-close"></i>';
         list.appendChild(newList);
+        localStorage.setItem('myNots', JSON.stringify(notes));
     }
     notesInput.value = '';
-    localStorage.setItem('myNots', JSON.stringify(notes));
+    // var xy = local_notes.concat(notes);
+    // console.log(xy);
+    // localStorage.setItem('my', JSON.stringify(notes));
+
 }
 
 
 
 
 window.onclick = e => {
-    if (e.target.className == 'fa fa-close') {
-        e.target.parentElement.remove();
-    }
+    // if (e.target.className == 'fa fa-close') {
+    //     e.target.parentElement.remove();
+    // }
+    // console.log(e.target.innerText);
 }
